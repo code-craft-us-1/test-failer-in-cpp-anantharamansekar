abe5d736b6b92ec728f1d56887fb43eb3e1363dc
Sekar, Anantharaman <Anantharaman.Sekar@philips.com>
2024-09-16 15:21:04 +0530

    tshirts_corrected

--- tshirts.cpp
+++ tshirts.cpp
@@ -5,7 +5,7 @@ char size(int cms) {
     char sizeName = '\0';
     if (cms < 38) {
         sizeName = 'S';
-    } else if (cms > 38 && cms < 42) {
+    } else if (cms >= 38 && cms <= 42) {
         sizeName = 'M';
     } else if (cms > 42) {
         sizeName = 'L';
@@ -15,7 +15,9 @@ char size(int cms) {
 
 int main() {
     assert(size(37) == 'S');
+    assert(size(38) == 'M');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
