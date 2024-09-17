c19656c19adfb50addcf914c46e6439c6b8235f8
Sekar, Anantharaman <Anantharaman.Sekar@philips.com>
2024-09-17 14:40:36 +0530

    updated 2

--- misaligned.cpp
+++ misaligned.cpp
@@ -5,18 +5,15 @@
 const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
 const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
 
-std::string GetMajorColor(int n)
-{
+std::string GetMajorColor(int n){
     return majorColor[n];
 }
 
-std::string GetMinorColor(int n)
-{
+std::string GetMinorColor(int n){
     return minorColor[n];
 }
 
 int printColorMap() {
-
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         std::string majorColor = GetMajorColor(i);
